d6da91b83564f0f58dac410f8774a299ea502bb0
Guilherme Abel <guilhermeabel8@gmail.com>
2023-04-14 09:05:29 -0300

    feat: improve backend endpoints

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai';
 import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
@@ -10,12 +12,41 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-(async () => {
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/listModels', async (req, res) => {
 	try {
 		const response = await openai.listModels();
 		const modelNames = response.data.data.map((model) => model.id);
-		console.log('Model names:', modelNames);
+		res.json(modelNames);
 	} catch (error) {
-		console.error('Error:', error.message);
+		res.status(500).json({ error: error.message });
 	}
-})();
+});
+
+app.post('/sendRequest', async (req, res) => {
+	const { message } = req.body;
+	try {
+		const response = await openai.createCompletion({
+			model: 'gpt-3.5-turbo',
+			messages: [
+				{
+					role: 'user',
+					content: message,
+				},
+			],
+		});
+		res.json(response.data.choices[0].text);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+});
+
+app.listen(port, () => {
+	console.log(`App listening at http://localhost:${port}`);
+});
